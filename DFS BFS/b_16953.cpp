a2c7c592832718173b2f7b4d5e2ef985d5e81352
hjinha18 <75845861+hjinha18@users.noreply.github.com>
2021-03-30 22:26:49 +0900

    Update b_16953.cpp

--- DFS BFS/b_16953.cpp
+++ DFS BFS/b_16953.cpp
@@ -12,4 +12,43 @@ A를 B로 바꾸는데 필요한 연산의 최솟값을 구해보자.
 A를 B로 바꾸는데 필요한 연산의 최솟값에 1을 더한 값을 출력한다. 만들 수 없는 경우에는 -1을 출력한다.
 */
 
+#include<iostream>
+#include<queue>
+using namespace std;
 
+//int graph[1000000000]; //범위가..
+
+void bfs(int a, int b) {
+	queue<pair<int, int>> q;
+
+	q.push({ b, 1 });
+	while (!q.empty()) {
+		int cur = q.front().first;
+		int n = q.front().second;
+		q.pop();
+		//2로 나눠지면 나누기
+		if (cur % 2 == 0) {
+			if (cur / 2 == a) {
+				cout << n + 1;
+				return;
+			}
+			else if (cur / 2 > a)
+				q.push({ cur / 2, n + 1 });
+		}
+		if (cur % 10 == 1) {
+			if (cur / 10 == a) {
+				cout << n + 1;
+				return;
+			}
+			else if (cur / 10 > a)
+				q.push({ cur / 10, n + 1 });
+		}
+	}
+	cout << -1;
+}
+
+int main() {
+	int a, b;
+	cin >> a >> b;
+	bfs(a, b);
+}
