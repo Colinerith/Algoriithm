c321be11fbabfbf486d5d5f3e4f5879c25230e67
hjinha18 <75845861+hjinha18@users.noreply.github.com>
2021-04-16 17:08:48 +0900

    Create b_11722.cpp

---
+++ DP/b_11722.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+int a[1001];
+int d[1001];
+
+int main() {
+	ios::sync_with_stdio(false);
+	int n;
+	cin >> n;
+	for (int i = 1; i <= n; i++) {
+		cin >> a[i];
+	}
+	d[1] = 1;
+	for (int i = 2; i <= n; i++) {
+		d[i] = 1;
+		for (int j = 1; j < i; j++) {
+			if (a[i] < a[j] && d[i] < d[j] + 1)
+				d[i] = d[j] + 1;
+		}
+	}
+	int max = 0;
+	for (int i = 1; i <= n; i++) {
+		if (d[i] > max)
+			max = d[i];
+	}
+	cout << max;
+}
