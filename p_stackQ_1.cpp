ea117e8389d654553f00b5f57ee154ffa3c16666
hjinha18 <75845861+hjinha18@users.noreply.github.com>
2021-09-06 23:24:06 +0900

    Create p_stackQ_1.cpp

---
+++ p_stackQ_1.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+vector<int> solution(vector<int> progresses, vector<int> speeds) {
+	vector<int> answer;
+
+
+
+	return answer;
+}
+
+int main() {
+	vector<int> p = { 95, 90, 99, 99, 80, 99 };
+	vector<int> s = { 1, 1, 1, 1, 1, 1 };
+
+	vector<int> a = solution(p, s);
+}
