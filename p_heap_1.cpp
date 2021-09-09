4b276233620ebe868c4aff0f5766a1be29e56a44
hjinha18 <75845861+hjinha18@users.noreply.github.com>
2021-09-09 23:50:15 +0900

    Update p_heap_1.cpp

--- p_heap_1.cpp
+++ p_heap_1.cpp
@@ -1,1 +1,12 @@
-#include<iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> scoville, int K) {
+    int answer = 0;
+    
+    
+    
+    return answer;
+}
