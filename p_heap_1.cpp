4771ba380cc96b69fbd8dba40290ab620a9f47ad
hjinha18 <75845861+hjinha18@users.noreply.github.com>
2021-09-10 00:25:49 +0900

    Update p_heap_1.cpp

--- p_heap_1.cpp
+++ p_heap_1.cpp
@@ -1,12 +1,25 @@
 #include <string>
 #include <vector>
+#include <queue>
 
 using namespace std;
 
 int solution(vector<int> scoville, int K) {
-    int answer = 0;
-    
-    
-    
-    return answer;
+	int answer = 0;
+	priority_queue<int, vector<int>, greater<int>> pq;
+	for (int i = 0; i < scoville.size(); i++)
+		pq.push(scoville[i]);
+
+	while (pq.top() < K) {
+		if (pq.size() == 1)
+			return -1;
+		answer++;
+		int a = pq.top();
+		pq.pop();
+		a += pq.top()*2;
+		pq.pop();
+		pq.push(a);
+	}
+
+	return answer;
 }
