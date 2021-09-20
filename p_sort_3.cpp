1b1031671117a3e87de1cd2000e6a6ff5ba0ece4
hjinha18 <75845861+hjinha18@users.noreply.github.com>
2021-09-21 01:28:36 +0900

    Update p_sort_3.cpp

--- p_sort_3.cpp
+++ p_sort_3.cpp
@@ -1,9 +1,14 @@
 #include <string>
 #include <vector>
-
+#include <algorithm>
 using namespace std;
 
 int solution(vector<int> citations) {
-    int answer = 0;
-    return answer;
+    int s = citations.size();
+    sort(citations.begin(), citations.end());
+    int tmp = 0;
+    for(int i=s-1;i>=0;i--){
+        if(citations[i] == s-i) return citations[i];
+        else if(citations[i] < s-i) return s-i - 1;
+    }
 }
