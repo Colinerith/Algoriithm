a80073c15ef83afe618eb3e855077ed0dff3dabd
hjinha18 <75845861+hjinha18@users.noreply.github.com>
2021-09-23 01:35:10 +0900

    Update p_stackQ_2.cpp

--- p_stackQ_2.cpp
+++ p_stackQ_2.cpp
@@ -1,3 +1,6 @@
+// pq 대신 algorithm의 max_element 함수를 쓰는 방법도 있음.
+// int max = *max_element(priorities.begin(), priorities.end());
+
 #include <string>
 #include <vector>
 #include <queue>
@@ -5,27 +8,29 @@
 using namespace std;
 
 int solution(vector<int> priorities, int location) {
-    int answer = 0;
-    int number = priorities[location];
-    priority_queue<int> pq;
-    for(int i=0;i<priorities.size();i++)
-        pq.push(priorities[i]);
-    
-    int idx = 0;
-    while(!pq.empty()){
-        if(priorities[idx] == number){
-            return answer;
-        }
-        if(pq.top() != priorities[idx]){
-            int tmp = priorities[idx];
-            priorities.push_back(tmp);
-        }
-        else{
-            pq.pop();
-        }
-        idx++;
-        number++;
-    }
-    
-    //return answer;
+	int answer = 1;
+	int number = priorities[location];
+	priority_queue<int> pq;
+	vector<pair<int, int>> v;
+
+	for (int i = 0; i < priorities.size(); i++) {
+		v.push_back({i, priorities[i]});
+		pq.push(priorities[i]);
+	}
+
+	int idx = 0;
+	while (!pq.empty()) {
+		if (v[idx].second == pq.top()) {
+			if (v[idx].second == number && v[idx].first == location)
+				return answer;
+			pq.pop();
+			answer++;
+		}
+		else v.push_back({ v[idx].first, v[idx].second });
+		idx++;
+	}
+}
+
+int main() {
+	solution({ 1, 1, 9, 1, 1, 1 }, 0);
 }
