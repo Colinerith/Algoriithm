7d2d0c0aed10c059d499a3ca96e7c77f5e893b87
hjinha18 <75845861+hjinha18@users.noreply.github.com>
2021-09-25 23:33:16 +0900

    Create p_stackQ_3.cpp

---
+++ p_stackQ_3.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int bridge_length, int weight, vector<int> truck_weights) {
+    int answer = 0;
+    
+    for(int i=0;i<bridge_length;i++){
+        
+    }
+    
+    return answer;
+}
