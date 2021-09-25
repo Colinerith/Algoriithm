13bab7013ed3654a1209d148e142035041a5a8cb
hjinha18 <75845861+hjinha18@users.noreply.github.com>
2021-09-25 23:44:33 +0900

    Update p_stackQ_3.cpp

--- p_stackQ_3.cpp
+++ p_stackQ_3.cpp
@@ -1,14 +1,30 @@
 #include <string>
 #include <vector>
+#include <queue>
 
 using namespace std;
 
 int solution(int bridge_length, int weight, vector<int> truck_weights) {
-    int answer = 0;
-    
-    for(int i=0;i<bridge_length;i++){
+    int time = 0;
+    queue<pair<int, int>> onTheBridge;
+    for(int i=0;i<truck_weights.size();i++){
         
     }
+    int idx = 0;
+    int tempWeight = weight;
+    while(idx <= truck_weights.size()){
+        time++;
+        if(tempWeight >= truck_weights[idx]){
+            onTheBridge.push({truck_weights[idx],time});
+            tempWeight -= truck_weights[idx];
+            idx++;
+        }
+        if(time - onTheBridge.front().second == bridge_length){
+            tempWeight += onTheBridge.front().first;
+            onTheBridge.pop();
+        }
+    }
     
-    return answer;
+    
+    return time;
 }
