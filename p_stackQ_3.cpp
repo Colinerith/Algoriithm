529290fa17d15f2644f85104c9fb04f10161a914
hjinha18 <75845861+hjinha18@users.noreply.github.com>
2021-09-26 00:05:24 +0900

    Update p_stackQ_3.cpp

--- p_stackQ_3.cpp
+++ p_stackQ_3.cpp
@@ -5,26 +5,27 @@
 using namespace std;
 
 int solution(int bridge_length, int weight, vector<int> truck_weights) {
-    int time = 0;
-    queue<pair<int, int>> onTheBridge;
-    for(int i=0;i<truck_weights.size();i++){
-        
-    }
-    int idx = 0;
-    int tempWeight = weight;
-    while(idx <= truck_weights.size()){
-        time++;
-        if(tempWeight >= truck_weights[idx]){
-            onTheBridge.push({truck_weights[idx],time});
-            tempWeight -= truck_weights[idx];
-            idx++;
-        }
-        if(time - onTheBridge.front().second == bridge_length){
-            tempWeight += onTheBridge.front().first;
-            onTheBridge.pop();
-        }
-    }
-    
-    
-    return time;
+	int time = 0;
+	queue<pair<int, int>> onTheBridge;
+	int idx = 0;
+	int tempWeight = weight;
+	while (true) {
+		time++;
+		if (!onTheBridge.empty() && time - onTheBridge.front().second == bridge_length) {
+			tempWeight += onTheBridge.front().first;
+			onTheBridge.pop();
+			if (idx == truck_weights.size() && onTheBridge.empty())
+				return time;
+		}
+		if (idx < truck_weights.size() && tempWeight >= truck_weights[idx]) {
+			onTheBridge.push({ truck_weights[idx],time });
+			tempWeight -= truck_weights[idx];
+			idx++;
+		}
+	}
+	return time;
+}
+
+int main() {
+	solution(100, 100, { 10, 10, 10, 10, 10, 10, 10, 10, 10, 10 });
 }
